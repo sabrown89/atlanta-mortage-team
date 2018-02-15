1bba6d51632c8a8111d8fc97efa7cc5ef3278d45
scotty <scottabrown89@gmail.com>
2018-02-15 20:04:56 +0000

    Fixes strftime syntax

--- blog/models.py
+++ blog/models.py
@@ -33,6 +33,6 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail', args=[self.publish.year,
-                                                 self.publish.strftime('%/m'),
-                                                 self.publish.strftime('%/d'),
+                                                 self.publish.strftime('%m'),
+                                                 self.publish.strftime('%d'),
                                                  self.slug])
