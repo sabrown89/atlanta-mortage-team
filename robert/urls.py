940a30beb2baac5b115e63575368d5d5eca24ede
scotty <scottabrown89@gmail.com>
2018-02-15 20:04:18 +0000

    Changes order in main urls

--- robert/urls.py
+++ robert/urls.py
@@ -3,13 +3,12 @@ from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path
 from . import views
-from blog import views as blog_views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    url(r'admin/', admin.site.urls),
     url(r'glossary/', views.glossary, name='glossary'),
     url(r'^blog/', include('blog.urls')),
-    url(r'admin/', admin.site.urls),
+    url(r'^$', views.index, name='index'),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
